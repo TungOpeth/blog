f9615ab9547e6626b65524da6ac78b9dd4195b8b
Dinh Tung <dinh.tung@framgia.com>
2015-10-21 11:28:47 +0700

    Add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,13 @@
 class StaticPagesController < ApplicationController
+include SessionsHelper
   def home
+	  	if logged_in?
+	  		@user = current_user
+	  		@entry = current_user.entries.build
+      		@feed_items = current_user.feed.paginate(page: params[:page])
+	  	else
+	  		@entries = Entry.all.paginate(page: params[:page])
+	  	end
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
   root 'static_pages#home'
   get  'static_pages/help'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :users
+  resources :entries, only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
